3ee3a990c1f01bbfd819fc50b3d4f72e93a7c03d
Kalanzifrank1 <kalanzifrank5@gmail.com>
2025-03-26 06:12:25 -0700

    second commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose'
+import MyUserRoutes from './routes/MyUserRoutes';
+
+mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING as string)
+    .then(() => console.log('Connected to MongoDB'))
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({message: "health ok!"})
+})
+
+app.use("/api/my/user", MyUserRoutes)
+
+app.get('/test', async (req: Request, res: Response) => {
+    res.json({ message: "hello" })
+})
+
+app.listen(7000, () => {
+    console.log("server started")
+})
